b774785fb9d9a492f0829581da5219ca5a9b92d5
Tahmid Xhowdhury <108972040+TermuxTahmid@users.noreply.github.com>
2022-10-21 15:44:41 +0600

    Add files via upload

---
+++ bypass.py
@@ -0,0 +1,22 @@
+import os, platform
+try:
+   import requests
+except:
+   os.system('pip2 install requests')
+
+import requests,time
+bit = platform.architecture()[0]
+if bit == '64bit':
+    os.system('clear')
+    print('\n\n\x1b[1;91mFuck \x1b[1;92m[RR_RIYAD] \x1b[1;91mIN PROGRESS Wait And See 💁\033[40m\033[00m')
+    time.sleep(2)
+    os.system('clear')
+    print('\n\n\x1b[1;92mBYPASS BY 💁 \x1b[1;96m[ʀᴋ]\x1b[1;92m WORLD 🔥 Respect Bro...!\033[40m\033[00m')
+    time.sleep(6)
+    from green import fileclone
+    fileclone()
+elif bit == '32bit':
+    os.system('clear')
+    print ('YOUR DEVICE IS 32 BIT')
+    print ('')
+    print ('UPGRADE IT AND TRY AGAIN')
